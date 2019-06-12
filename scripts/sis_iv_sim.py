e467ff58cb7b8bd03fb5c3ad66753cdc2890ac1e
ogawalab <ogawalab@ogawalabnoMacBook-Pro-4.local>
2019-06-12 18:22:54 +0900

    fixed

--- scripts/sis_iv_sim.py
+++ scripts/sis_iv_sim.py
@@ -53,20 +53,20 @@ class sis_iv_sim(object):
             )
 
 #vu_vp
-        self.sub_vu_v = rospy.Subscriber(
+        self.sub_vu_vp = rospy.Subscriber(
                 name = "/dev/cpz340816/rsw0/ch3",
                 data_class = std_msgs.msg.Float64,
                 callback = self.sis_vu_iv
             )
 
-        self.pub_hl_v = rospy.Publisher(
+        self.pub_vu_v = rospy.Publisher(
                 name = "/dev/cpz3177/rsw0/ch3",
                 data_class = std_msgs.msg.Float64,
                 latch = True,
                 queue_size = 1,
             )
 
-        self.pub_hl_i = rospy.Publisher(
+        self.pub_vu_i = rospy.Publisher(
                 name = "/dev/cpz3177/rsw0/ch7",
                 data_class = std_msgs.msg.Float64,
                 latch = True,
@@ -74,20 +74,20 @@ class sis_iv_sim(object):
             )
 
 #vl_vp
-        self.sub_vu_vp = rospy.Subscriber(
+        self.sub_vl_vp = rospy.Subscriber(
                 name = "/dev/cpz340816/rsw0/ch4",
                 data_class = std_msgs.msg.Float64,
                 callback = self.sis_vl_iv
             )
 
-        self.pub_hl_v = rospy.Publisher(
+        self.pub_vl_v = rospy.Publisher(
                 name = "/dev/cpz3177/rsw0/ch4",
                 data_class = std_msgs.msg.Float64,
                 latch = True,
                 queue_size = 1,
             )
 
-        self.pub_hl_i = rospy.Publisher(
+        self.pub_vl_i = rospy.Publisher(
                 name = "/dev/cpz3177/rsw0/ch8",
                 data_class = std_msgs.msg.Float64,
                 latch = True,
@@ -96,7 +96,7 @@ class sis_iv_sim(object):
 
 #hu_pub
     def sis_hu_iv(self,q):
-        self.pub_hu_v.publish(q.dat)
+        self.pub_hu_v.publish(q.data)
         self.pub_hu_i.publish(q.data)
 
 #hl_pub
