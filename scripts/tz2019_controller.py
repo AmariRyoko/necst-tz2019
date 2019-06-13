753eb54fd7c6d420e4445eb9078241e1da2485d7
ogawalab <ogawalab@ogawalab-no-MacBook-Air.local>
2019-06-13 16:12:31 +0900

    bug fix tz_controller

--- scripts/tz2019_controller.py
+++ scripts/tz2019_controller.py
@@ -39,7 +39,7 @@ class PS(object):
 class SWITCH(object):
 
     def __init__(self):
-        self.pub = {}
+        rospy.init_node(name)
         self.ps = PS()
 
     def if_switch(self, command):
@@ -53,7 +53,7 @@ class SWITCH(object):
 class LOSG(object):
 
     def __init__(self):
-        self.pub = {}
+        rospy.init_node(name)
         self.ps = PS()
 
     def losg_f(self, command):
