683358ff756142d78e443afd1c4e6af6cb2c16d4
tsutsumi-toshihisa <toshi.toshi.223@gmail.com>
2019-06-14 10:18:25 +0900

    fixed

---
+++ scripts/sa_data_sim.py
@@ -0,0 +1,41 @@
+#topic(/dev/n9343c)にデータを投げるだけのsimulator用script(スペアナ用シミュレータ)
+name = "sa_data_sim"
+
+import time
+import rospy
+import std_msgs.msg
+import threading
+
+
+class sa_data_sim(object):
+
+    def __init__(self):
+
+
+        self.pub_sa_data = rospy.Publisher(
+                name = "/dev/n9343/",
+                data_class = std_msgs.msg.Float64,
+                latch = True,
+                queue_size = 1,
+            )
+
+    def data(self):
+        while not rospy.is_shutdown():
+            self.pub_sa_data.publish(1)
+            time.sleep(1)
+            continue
+
+    def thread(self):
+        th = threading.Thread(target=self.data)
+        th.setDaemon(True)
+        th.start()
+
+
+
+if __name__ == "__main__":
+
+    rospy.init_node(name)
+    sa = sa_data_sim()
+    sa.thread()
+    sa.data()
+    rospy.spin()
