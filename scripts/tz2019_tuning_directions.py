f9f1068162af45201eafc41ad6a3c9439e062401
ogawalab <ogawalab@ogawalab-no-MacBook-Air.local>
2019-06-21 14:55:43 +0900

    add tz2019-tuning directions

--- scripts/tz2019_tuning_directions.py
+++ scripts/tz2019_tuning_directions.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 
 name = 'tz2019_tuning_directions'
+
+import rospy
+import time
+import numpy
+import std_msgs.msg
+
+import tz2019_tuning_controler
+
+rospy.init_node(name)
+
+loatt_tuning = tz2019_tuning_controler.loatt_tuning()
+
+loatth_tuning = input("Lo h-band Att tuning vol: ")
+loattv_tuning = input("Lo v-band Att tuning vol: ")
+
+loatt_tuning.set_loatth_tuning(loatth_tuning)
+loatt_tuning.set_loattv_tuning(loattv_tuning)
